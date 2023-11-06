00586a874c0c6cade46f6341a318d9ceba78abaf
Ziyad Akchouch <zakchouc@bess-f4r2s13.clusters.42paris.fr>
2023-11-06 12:30:30 +0100

    isascii done

---
+++ ft_isascii.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakchouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 12:00:59 by zakchouc          #+#    #+#             */
+/*   Updated: 2023/11/06 12:30:14 by zakchouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isascii(int c)
+{
+	if ((c >= 0) && (c <= 127))
+		return (1);
+	else
+		return (0);
+}
+
+// int	main(void)
+// {
+// 	printf("isascii %c : %d\n", 'w', ft_isascii('w'));
+// 	printf("isascii %c : %d\n", ' ', ft_isascii(' '));
+// 	// printf("isascii %c : \n", '£'); //, ft_isascii("£"));
+// 	// printf("isascii %c : \n", 'í'); //, ft_isascii("í"));
+// 	// printf("isascii %c : \n", 'é'); //, ft_isascii("é"));
+// 	printf("isascii %c : %d\n", '/', ft_isascii('/'));
+// 	printf("isascii %c : %d\n", '@', ft_isascii('@'));
+// 	return (0);
+// }
