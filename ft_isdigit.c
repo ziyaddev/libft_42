1dcb4597f977437a2a5792e93e2ae32cb563eac7
Ziyad Akchouch <zakchouc@bess-f4r2s13.clusters.42paris.fr>
2023-11-06 11:52:59 +0100

    isdigit done

---
+++ ft_isdigit.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakchouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 11:47:28 by zakchouc          #+#    #+#             */
+/*   Updated: 2023/11/06 11:51:32 by zakchouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	else
+		return (0);
+}
+
+// int	main(void)
+// {
+// 	printf("is digit : %d\n", ft_isdigit('2'));
+// 	printf("is digit : %d\n", ft_isdigit('?'));
+// 	printf("is digit : %d\n", ft_isdigit('0'));
+// 	printf("is digit : %d\n", ft_isdigit('\''));
+// 	printf("is digit : %d\n", ft_isdigit('a'));
+// 	printf("is digit : %d\n", ft_isdigit(' '));
+// 	printf("is digit : %d\n", ft_isdigit('9'));
+// 	return (0);
+// }
