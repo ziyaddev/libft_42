13db698bd40d365507871298dc12c356bc0ddf61
Ziyad Akchouch <zakchouc@bess-f4r2s13.clusters.42paris.fr>
2023-11-06 12:43:06 +0100

    isprint done

---
+++ ft_isprint.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakchouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 12:31:03 by zakchouc          #+#    #+#             */
+/*   Updated: 2023/11/06 12:42:47 by zakchouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isprint(int c)
+{
+	if ((c >= 32) && (c <= 126))
+		return (1);
+	else
+		return (0);
+}
+
+// int	main(void)
+// {
+// 	printf("ft_isprint %c : %d\n", 's', ft_isprint('s'));	
+// 	printf("ft_isprint %c : %d\n", ' ', ft_isprint(' '));	
+// 	printf("ft_isprint %c : %d\n", '\'', ft_isprint('\''));	
+// 	printf("ft_isprint %c : %d\n", 12, ft_isprint(12));	
+// 	printf("ft_isprint %c : %d\n", '\n', ft_isprint('\n'));	
+// 	printf("ft_isprint %c : %d\n", '\t', ft_isprint('\t'));	
+// 	return (0);
+// }
