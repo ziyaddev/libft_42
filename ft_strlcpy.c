d7b60ac5ff663edee35f4c10b78bfc494000e801
Ziyad Akchouch <zakchouc@paul-f3Ar3s6.clusters.42paris.fr>
2023-11-08 18:38:05 +0100

    strlcpy done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,9 @@
 /*   By: zakchouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:52:40 by zakchouc          #+#    #+#             */
-/*   Updated: 2023/11/08 13:53:05 by zakchouc         ###   ########.fr       */
+/*   Updated: 2023/11/08 18:37:07 by zakchouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
-#include <bsd/string.h>
 
 int	ft_strlcpy(char *dest, const char *src, size_t size)
 {
@@ -23,14 +19,49 @@ int	ft_strlcpy(char *dest, const char *src, size_t size)
 	{
 		dest[len] = src[len];
 		len++;
+		if (!size)
+			dest[len] = 0;
 	}
-	
+	len = 0;
+	while (src[len])
+		len++;
 	return (len);
 }
 
-int	main(void)
-{
-	printf("strlcpy : %d\n" ft_strlcpy());
-
-	return (0);
-}
+// #include <stdio.h>
+// #include <string.h>
+// #include <bsd/string.h>
+// int	main(void)
+// {
+// 	size_t size;
+// 	char	data_src[] = "salam";
+// 	char	data_dest[50];
+//
+// 	char	data_src_ori[] = "salam";
+// 	char	data_dest_ori[50];
+// 	int	i;
+//
+// 	size = 6;
+// 	i = 3;
+// 	printf("\tft_strlcpy | strlcpy ori\n");
+//
+// 	printf("  data_src : %s | ", data_src);
+// 	printf("%s : data_src_ori\n", data_src_ori);
+//
+// 	printf("      data_dest : %s | ", data_dest);
+// 	printf("%s : data_dest_ori\n", data_dest_ori);
+//
+// 	printf("ft_strlcpy : %d\n", ft_strlcpy(data_dest, data_src, size));
+// 	printf("strlcpy ori : %zu\n\n", strlcpy(data_dest_ori, data_src_ori, size));
+//
+// 	printf("  data_src : %s | ", data_src);
+// 	printf("%s : data_src_ori\n", data_src_ori);
+//
+// 	printf(" data_dest : %s | ", data_dest);
+// 	printf("%s : data_dest_ori\n", data_dest_ori);
+//
+// 	printf(" data_dest : %d | ", data_dest[i]);
+// 	printf("%d : data_dest_ori\n", data_dest_ori[i]);
+//
+// 	return (0);
+// }
