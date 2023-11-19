65383055bf568f91e75b11b1559890ad19136dd4
Ziyad Akchouch <zakchouc@paul-f3Ar3s10.clusters.42paris.fr>
2023-11-19 12:04:49 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "libft.h"
+
+
+int	main(int argc, char *argv[])
+{
+	// int	len;
+	int i;
+	// char sub[] = "ll";
+	// char sub[10] = argv[1];
+
+	i = 0;
+	// len = 4;
+	// len = atoi(argv[1]);
+	if (argc < 1)
+		return (0);
+	(void)argv; //atoi(argv[1])
+
+	while (i++ < 15)
+	{
+		// printf("\nsub : \"%s\"\tlen %d\tft strnstr   : %s\n", sub, i,
+		// 		ft1_strnstr("salamhellosalut", sub, i));
+		// printf("sub : \"%s\"\tlen %d\tstrnstr ori  : %s\n", sub, i,
+		// 		strnstr("salamhellosalut", sub, i));
+
+		printf("\nsub : \"%s\"\tlen %d\tft strnstr   : %s\n", argv[1], i,
+				ft1_strnstr("salamhellosalut", argv[1], i));
+		printf("sub : \"%s\"\tlen %d\tstrnstr ori  : %s\n", argv[1], i,
+				strnstr("salamhellosalut", argv[1], i));
+	}
+	return (0);
+}
