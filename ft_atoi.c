b4caf489e3e1ec200695785edc665bf83d728ffc
Ziyad Akchouch <zakchouc@paul-f3Ar3s11.clusters.42paris.fr>
2023-11-23 08:17:29 +0100

    atoi done

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,39 +11,51 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_atoi(const char *nptr)
 {
 	int	result;
 	int	i;
+	int	sign;
 
 	result = 0;
 	i = 0;
+	sign = 1;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || (nptr[i] == 32))
+		i++;
 	if (nptr[i] == '-')
 	{
 		i++;
-		while (nptr[i])
-		{
-			result = result * 10 + (nptr[i] - '0');
-			if ((nptr[i] < '0' || nptr[i] > '9'))
-				return (0);
-			i++;
-		}
-		result *= -1;
+		sign *= -1;
 	}
+	else if (nptr[i] == '+')
+		i++;
 	while (nptr[i])
 	{
+		if (((nptr[i] < '0') || (nptr[i] > '9')))
+			return (result * sign);
 		result = result * 10 + (nptr[i] - '0');
-		if ((nptr[i] < '0' || nptr[i] > '9'))
-			return (0);
 		i++;
 	}
-	return (result);
+	return (result * sign);
 }
 
 // int	main(void)
 // {
+// 	printf("--------\n");
+// 	printf("atoi ft  : %d\n", ft_atoi("-0"));
+// 	printf("atoi ori : %d\n\n", atoi("-0"));
+// 	printf("atoi ft  : %d\n", ft_atoi("-123"));
+// 	printf("atoi ori : %d\n\n", atoi("-123"));
+// 	printf("atoi ft  : %d\n", ft_atoi("-4294967296"));
+// 	printf("atoi ori : %d\n\n", atoi("-4294967296"));
+// 	printf("atoi ft  : %d\n", ft_atoi("-2147483647"));
+// 	printf("atoi ori : %d\n\n", atoi("-2147483647"));
+// 	printf("atoi ft  : %d\n", ft_atoi("-2147483648"));
+// 	printf("atoi ori : %d\n\n", atoi("-2147483648"));
+// 	printf("atoi ft  : %d\n", ft_atoi("-a2147483649"));
+// 	printf("atoi ori : %d\n\n\n", atoi("-a2147483649"));
+//
 // 	printf("atoi ft  : %d\n", ft_atoi("0"));
 // 	printf("atoi ori : %d\n\n", atoi("0"));
 // 	printf("atoi ft  : %d\n", ft_atoi("123"));
@@ -56,20 +68,36 @@ int	ft_atoi(const char *nptr)
 // 	printf("atoi ori : %d\n\n", atoi("2147483648"));
 // 	printf("atoi ft  : %d\n", ft_atoi("2147483649"));
 // 	printf("atoi ori : %d\n\n\n", atoi("2147483649"));
-// 	printf("atoi ft  : %d\n", ft_atoi("a2147483649"));
-// 	printf("atoi ori : %d\n\n\n", atoi("a2147483649"));
+// 	printf("atoi ft  : %d\n", ft_atoi("555a67"));
+// 	printf("atoi ori : %d\n\n\n", atoi("555a67"));
+// 	printf("atoi ft  : %d\n", ft_atoi("+-55567"));
+// 	printf("atoi ori : %d\n\n\n", atoi("+-55567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("-+55567"));
+// 	printf("atoi ori : %d\n\n\n", atoi("-+55567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("555:67"));
+// 	printf("atoi ori : %d\n\n", atoi("555:67"));
+// 	printf("atoi ft  : %d\n", ft_atoi("055567"));
+// 	printf("atoi ori : %d\n\n", atoi("055567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("000567"));
+// 	printf("atoi ori : %d\n\n", atoi("000567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("--055567"));
+// 	printf("atoi ori : %d\n\n", atoi("--055567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("++055567"));
+// 	printf("atoi ori : %d\n\n", atoi("++055567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("0"));
+// 	printf("atoi ori : %d\n\n", atoi("0"));
+// 	printf("atoi ft  : %d\n", ft_atoi("+055567"));
+// 	printf("atoi ori : %d\n\n", atoi("+055567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("\e055567"));
+// 	printf("atoi ori : %d\n\n", atoi("\e055567"));
+// 	printf("atoi ft  : %d\n", ft_atoi("\t\n\r\v\f  469 \n"));
+// 	printf("atoi ori : %d\n\n", atoi("\t\n\r\v\f  469 \n"));
+// 	printf("atoi ft  : %d\n", ft_atoi("\t\n\r\v\fd469 \n"));
+// 	printf("atoi ori : %d\n\n", atoi("\t\n\r\v\fd469 \n"));
+// 	printf("atoi ft  : %d\n", ft_atoi(""));
+// 	printf("atoi ori : %d\n\n", atoi(""));
+// 	printf("atoi ft  : %d\n", ft_atoi("\n\n\n  -46\b9 \n5d6"));
+// 	printf("atoi ori : %d\n\n", atoi("\n\n\n  -46\b9 \n5d6"));
 //
-// 	printf("atoi ft  : %d\n", ft_atoi("-0"));
-// 	printf("atoi ori : %d\n\n", atoi("-0"));
-// 	printf("atoi ft  : %d\n", ft_atoi("-123"));
-// 	printf("atoi ori : %d\n\n", atoi("-123"));
-// 	printf("atoi ft  : %d\n", ft_atoi("-4294967296"));
-// 	printf("atoi ori : %d\n\n", atoi("-4294967296"));
-// 	printf("atoi ft  : %d\n", ft_atoi("-2147483647"));
-// 	printf("atoi ori : %d\n\n", atoi("-2147483647"));
-// 	printf("atoi ft  : %d\n", ft_atoi("-2147483648"));
-// 	printf("atoi ori : %d\n\n", atoi("-2147483648"));
-// 	printf("atoi ft  : %d\n", ft_atoi("-a2147483649"));
-// 	printf("atoi ori : %d\n\n", atoi("-a2147483649"));
 // 	return (0);
 // }
