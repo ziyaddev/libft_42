a7fc0227e0163ade0f82f2ab00e19b132b2d1644
Ziyad Akchouch <zakchouc@paul-f3Ar3s11.clusters.42paris.fr>
2023-11-23 13:35:21 +0100

    libft.h reorder

--- libft.h
+++ libft.h
@@ -17,7 +17,11 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+int		ft_atoi(const char *nptr);
+
 void	ft_bzero(void *s, size_t n);
+
+void	*ft_calloc(size_t nmemb, size_t size);
 
 int		ft_isalnum(int c);
 
@@ -41,27 +45,22 @@ void	*ft_memset(void *ptr, int value, size_t count);
 
 char	*ft_strchr(const char *s, int c);
 
-int		ft_toupper(int c);
-
-size_t	ft_strlen(const char *str);
-
-int		ft_strncmp(const char *first, const char *second, size_t length);
-
-char	*ft_strrchr(const char *s, int c);
-
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-
-int		ft_tolower(int c);
-
-int		ft_toupper(int c);
-
-int		ft_atoi(const char *nptr);
+char	*ft_strdup(const char *s);
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
-char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *str);
 
+int		ft_strncmp(const char *first, const char *second, size_t length);
+
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+
+char	*ft_strrchr(const char *s, int c);
+
+int		ft_tolower(int c);
+
+int		ft_toupper(int c);
 
 #endif
