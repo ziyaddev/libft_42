7458fb4d2728b83bbfdae667386505a864751436
Ziyad Akchouch <zakchouc@paul-f3Ar3s11.clusters.42paris.fr>
2023-11-23 18:41:06 +0100

    strlcpy in progress...

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -11,6 +11,9 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <bsd/string.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
@@ -30,40 +33,35 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (len);
 }
 
-// #include <stdio.h>
-// #include <string.h>
-// #include <bsd/string.h>
-// int	main(void)
-// {
-// 	size_t size;
-// 	char	data_src[] = "salam";
-// 	char	data_dest[50];
-//
-// 	char	data_src_ori[] = "salam";
-// 	char	data_dest_ori[50];
-// 	int	i;
-//
-// 	size = 6;
-// 	i = 3;
-// 	printf("\tft_strlcpy | strlcpy ori\n");
-//
-// 	printf("  data_src : %s | ", data_src);
-// 	printf("%s : data_src_ori\n", data_src_ori);
-//
-// 	printf("      data_dest : %s | ", data_dest);
-// 	printf("%s : data_dest_ori\n", data_dest_ori);
-//
-// 	printf("ft_strlcpy : %d\n", ft_strlcpy(data_dest, data_src, size));
-// 	printf("strlcpy ori : %zu\n\n", strlcpy(data_dest_ori, data_src_ori, size));
-//
-// 	printf("  data_src : %s | ", data_src);
-// 	printf("%s : data_src_ori\n", data_src_ori);
-//
-// 	printf(" data_dest : %s | ", data_dest);
-// 	printf("%s : data_dest_ori\n", data_dest_ori);
-//
-// 	printf(" data_dest : %d | ", data_dest[i]);
-// 	printf("%d : data_dest_ori\n", data_dest_ori[i]);
-//
-// 	return (0);
-// }
+int	main(void)
+{
+	size_t	size;
+	char	data_src[] = "salam";
+	char	data_dest[5];
+
+	char	data_src_ori[] = "salam";
+	char	data_dest_ori[5];
+	int		i;
+
+	size = 6;
+	i = 3;
+	printf("data_src_ft   : %s\n", data_src);
+	printf("data_src_ori  : %s\n\n", data_src_ori);
+
+	printf("data_dest_ft  : %s\n", data_dest);
+	printf("data_dest_ori : %s\n\n", data_dest_ori);
+
+	printf("ft_strlcpy    : %zu\n", ft_strlcpy(data_dest, data_src, size));
+	printf("strlcpy ori   : %zu\n\n", strlcpy(data_dest_ori, data_src_ori, size));
+
+	printf("data_src_ft   : %s\n", data_src);
+	printf("data_src_ori  : %s\n\n", data_src_ori);
+
+	printf("data_dest_ft  : %s\n", data_dest);
+	printf("data_dest_ori : %s\n\n", data_dest_ori);
+
+	printf("data_dest_ft  : %d\n", data_dest[i]);
+	printf("data_dest_ori : %d\n", data_dest_ori[i]);
+
+	return (0);
+}
