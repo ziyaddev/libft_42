edef9c058b6866e992165ee5f50631d2d51e16d1
Ziyad Akchouch <zakchouc@bess-f1r3s11.clusters.42paris.fr>
2023-11-28 20:06:24 +0100

    done..

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -58,21 +58,14 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 
 	len = 0;
 	src_len = ft_strlen(src);
-	// if (!dest)
-	// 	return (src_len);
-	// Let's start with size = 4
-	// Take position at end of dst
 	while (*dest && size > 0)
 	{
 		dest++;
 		len++;
 		size--;
 	}
-	// size = 2 & len = 2
-
 	while (*src && size-- > 1)
 		*dest++ = *src++;
-	// size = 1 & len = 2
 	if (size == 1 || *src == 0)
 		*dest = '\0';
 	return (src_len + len);
@@ -104,100 +97,104 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 // 	return (0);
 // }
 //
-#define DEST_BUF 100
-
-int	main(void)
-{
-	size_t	size;
-	char	destBuffer_ft[DEST_BUF];
-	char	srcBuffer_ft[256];
-	char	destBuffer_ori[DEST_BUF];
-	char	srcBuffer_ori[256];
-	char	data_src[] = "my";
-	char	data_dest[10] = "string";
-	char	b[0xF] = "nyan !";
-
-	char	data_src_ori[] = "my";
-	char	data_dest_ori[10] = "string";
-	char	c;
-	int		i;
-	int		j;
-
-	size = 4;
-	i = 0;
-	j = 0;
-	while (j < 10)
-	{
-		// Put '/' in dest buffer
-		while (i < 6)
-			destBuffer_ft[i++] = '/';
-		i = 0;
-		// Put '/' in dest buffer
-		while (i < 6)
-			destBuffer_ori[i++] = '/';
-		i = 0;
-		// Clean ft src buffer
-		while (i < 256)
-			srcBuffer_ft[i++] = 0;
-		i = 0;
-		// Clean ori src buffer
-		while (i < 256)
-			srcBuffer_ori[i++] = 0;
-		i = 0;
-		// Fill-in random string in ft src buffer
-		while ((c = rand()%27+97) != 123)
-			srcBuffer_ft[i++] = c;
-		// Fill-in random string in ori src buffer
-		while ((c = rand()%27+97) != 123)
-			srcBuffer_ori[i++] = c;
-		printf("\n\n\nsrc ft buffer         : %s\n", srcBuffer_ft);
-		printf("src ft len            : %zu\n", ft_strlen(srcBuffer_ft));
-		printf("dest ft buffer before : %s\n", destBuffer_ft);
-
-		printf("\n\nsrc ori buffer         : %s\n", srcBuffer_ori);
-		printf("src ori len            : %zu\n", ft_strlen(srcBuffer_ori));
-		printf("dest ori buffer before : %s\n", destBuffer_ori);
-
-		ft_strlcat(destBuffer_ft, srcBuffer_ft, DEST_BUF);
-		printf("dest ft buffer after   : %s\n------------------>  ", destBuffer_ft);
-		for (size_t i = 0; i < DEST_BUF+2; i++)
-			printf(" %d",destBuffer_ft[i]);
-
-		strlcat(destBuffer_ori, srcBuffer_ori, DEST_BUF);
-		printf("\ndest ori buffer after  : %s\n------------------>  ", destBuffer_ori);
-		for (size_t i = 0; i < DEST_BUF+2; i++)
-			printf(" %d",destBuffer_ori[i]);
-		j++;
-	}
-	i = 6;
-	printf("\n\ndata_src_ft   : %s\n", data_src);
-	printf("data_src_ori  : %s\n\n", data_src_ori);
-
-	printf("data_dest_ft  : %s\n", data_dest);
-	printf("data_dest_ori : %s\n\n", data_dest_ori);
-
-	printf("ft_strlcat    : %zu\n", ft_strlcat(
-											destBuffer_ft,
-											b,
-											size));
-	printf("strlcat ori   : %zu\n\n", strlcat(
-											destBuffer_ft,
-											b,
-											size));
-
-	printf("data_src_ft   : %s\n", data_src);
-	printf("data_src_ori  : %s\n\n", data_src_ori);
-
-	printf("data_dest_ft  : %s\n", data_dest);
-	printf("data_dest_ori : %s\n\n", data_dest_ori);
-
-	printf("data_dest_ft  : ");
-	for (size_t i = 0; i < 13; i++)
-		printf(" %c", data_dest[i]);
-	printf("\ndata_dest_ori : ");
-	for (size_t i = 0; i < 13; i++)
-		printf(" %c", data_dest_ori[i]);
-
-	printf("\n-");
-	return (0);
-}
+// #define DEST_BUF 100
+//
+// int	main(void)
+// {
+// 	size_t	size;
+// 	char	destBuffer_ft[DEST_BUF];
+// 	char	srcBuffer_ft[256];
+// 	char	destBuffer_ori[DEST_BUF];
+// 	char	srcBuffer_ori[256];
+// 	char	data_src[] = "my";
+// 	char	data_dest[10] = "string";
+// 	char	b[0xF] = "nyan !";
+//
+// 	char	data_src_ori[] = "my";
+// 	char	data_dest_ori[10] = "string";
+// 	char	c;
+// 	int		i;
+// 	int		j;
+//
+// 	size = 2;
+// 	i = 0;
+// 	j = 0;
+// 	while (j < 10)
+// 	{
+// 		// Put '/' in dest buffer
+// 		while (i < 6)
+// 			destBuffer_ft[i++] = '/';
+// 		i = 0;
+// 		// Put '/' in dest buffer
+// 		while (i < 6)
+// 			destBuffer_ori[i++] = '/';
+// 		i = 0;
+// 		// Clean ft src buffer
+// 		while (i < 256)
+// 			srcBuffer_ft[i++] = 0;
+// 		i = 0;
+// 		// Clean ori src buffer
+// 		while (i < 256)
+// 			srcBuffer_ori[i++] = 0;
+// 		i = 0;
+// 		// Fill-in random string in ft src buffer
+// 		while ((c = rand()%27+97) != 123)
+// 			srcBuffer_ft[i++] = c;
+// 		// Fill-in random string in ori src buffer
+// 		while ((c = rand()%27+97) != 123)
+// 			srcBuffer_ori[i++] = c;
+// 		printf("\n\n\nsrc ft buffer         : %s\n", srcBuffer_ft);
+// 		printf("src ft len            : %zu\n",
+//				ft_strlen(srcBuffer_ft));
+// 		printf("dest ft buffer before : %s\n", destBuffer_ft);
+//
+// 		printf("\n\nsrc ori buffer         : %s\n", srcBuffer_ori);
+// 		printf("src ori len            : %zu\n",
+//				ft_strlen(srcBuffer_ori));
+// 		printf("dest ori buffer before : %s\n", destBuffer_ori);
+//
+//		ft_strlcat(destBuffer_ft, srcBuffer_ft, DEST_BUF);
+//		printf("dest ft buffer after   : %s\n------------------>  ",
+//				destBuffer_ft);
+//		for (size_t i = 0; i < DEST_BUF+2; i++)
+//			printf(" %c",destBuffer_ft[i]);
+//
+//		strlcat(destBuffer_ori, srcBuffer_ori, DEST_BUF);
+//		printf("\ndest ori buffer after  : %s\n------------------>  ",
+//				destBuffer_ori);
+//		for (size_t i = 0; i < DEST_BUF+2; i++)
+//			printf(" %c",destBuffer_ori[i]);
+// 		j++;
+// 	}
+// 	i = 6;
+// 	printf("\n\ndata_src_ft   : %s\n", data_src);
+// 	printf("data_src_ori  : %s\n\n", data_src_ori);
+//
+// 	printf("data_dest_ft  : %s\n", data_dest);
+// 	printf("data_dest_ori : %s\n\n", data_dest_ori);
+//
+// 	printf("ft_strlcat    : %zu\n", ft_strlcat(
+// 											NULL,
+// 											b,
+// 											size));
+// 	printf("strlcat ori   : %zu\n\n", strlcat(
+// 											NULL,
+// 											b,
+// 											size));
+//
+// 	printf("data_src_ft   : %s\n", data_src);
+// 	printf("data_src_ori  : %s\n\n", data_src_ori);
+//
+// 	printf("data_dest_ft  : %s\n", data_dest);
+// 	printf("data_dest_ori : %s\n\n", data_dest_ori);
+//
+// 	printf("data_dest_ft  : ");
+// 	for (size_t i = 0; i < 13; i++)
+// 		printf(" %c", data_dest[i]);
+// 	printf("\ndata_dest_ori : ");
+// 	for (size_t i = 0; i < 13; i++)
+// 		printf(" %c", data_dest_ori[i]);
+//
+// 	printf("\n-");
+// 	return (0);
+// }
