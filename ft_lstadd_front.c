ba1008a35eccf83aecf7d44c8008d62c8c6b7d85
Ziyad Akchouch <zakchouc@paul-f3Br7s1.clusters.42paris.fr>
2023-12-02 22:43:08 +0100

    lstadd_front init

---
+++ ft_lstadd_front.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakchouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 22:38:11 by zakchouc          #+#    #+#             */
+/*   Updated: 2023/12/02 22:38:56 by zakchouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file ft_lstadd_front.c
+ * @author Ziyad A. Dev (zakchouc@student.42.fr)
+ * @brief	Adds the node ’new’ at the beginning of the list.
+ * @param lst	The address of a pointer to the first link of a list.
+ * @param new	The address of a pointer to the node to be added to the list.
+ * @return	None
+ * @version 0.1
+ * @date 2023-12-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	
+}
+
+int	main(void)
+{
+	return (0);
+}
+
