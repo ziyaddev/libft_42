d1369a5637bd8d0ddb5b45cd4c58ced09e91791d
Ziyad Akchouch <zakchouc@paul-f3Br7s1.clusters.42paris.fr>
2023-12-03 18:56:06 +0100

    lstadd in progress...

---
+++ ft_lstadd_back.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakchouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 18:35:05 by zakchouc          #+#    #+#             */
+/*   Updated: 2023/12/03 18:36:08 by zakchouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file ft_lstadd_back.c
+ * @author Ziyad A. Dev (zakchouc@student.42.fr)
+ * @brief		Adds the node ’new’ at the end of the list.
+ * @param lst	The address of a pointer to the first link of a list.
+ * @param new	The address of a pointer to the node to be added to the list.
+ * @return 		None
+ * @version 0.1
+ * @date 2023-12-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	last_node = ft_lstlast(*lst);
+	last_node->next = new;
+}
+
+#include "ft_lstadd_front.c"
+#include "ft_lstnew.c"
+#include "ft_lstsize.c"
+#include "ft_lstlast.c"
+int	main(void)
+{
+	t_list	*my_list = ft_lstnew((void *)67);
+	ft_lstadd_front(&my_list, ft_lstnew((void *)33));
+	ft_lstadd_front(&my_list, ft_lstnew((void *)21));
+
+	printf("lst size : %d\n", ft_lstsize(my_list));
+
+	printf("last node : %p\n", ft_lstlast(my_list));
+
+	ft_lstadd_back(&my_list, ft_lstnew((void *)890));
+
+	printf("lst size after add back : %d\n", ft_lstsize(my_list));
+
+	printf("last node after add back: %p\n", ft_lstlast(my_list));
+
+	return (0);
+}
