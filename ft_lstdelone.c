ab0c2aea06d17be21cebaefef15d418260ec7ae5
Ziyad Ak <ziyad.dev@gmail.com>
2023-12-05 06:52:01 +0000

    lstdelone init

---
+++ ft_lstdelone.c
@@ -0,0 +1,28 @@
+
+/**
+ * @file ft_lstdelone.c
+ * @author Ziyad A. Dev (zakchouc@student.42.fr)
+ * @brief		Takes as a parameter a node and frees the memory of the node’s
+ * 				content using the function ’del’ given as a parameter and free
+ * 				the node. The memory of ’next’ must not be freed.
+ * 				Allowed function(s) : free
+ * @param lst	The node to free.
+ * @param del	The address of the function used to delete the content.
+ * @return		None
+ * @version 0.1
+ * @date 2023-12-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	t_list *;
+}
+
+int	main(void)
+{
+	return (0);
+}
